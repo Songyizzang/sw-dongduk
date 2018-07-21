7e9096fa7d76d16d99775147d66e96baedcd7685
BoramPark <ppr8769@likelion.org>
2018-07-21 06:17:53 +0000

    remove WebSocket

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,5 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :conversation
   
-  #보람 
-  after_create_commit { MessageBroadcastJob.perform_later(self) }
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,5 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  #보람
-  mount ActionCable.server => '/cable'
+  
 end
